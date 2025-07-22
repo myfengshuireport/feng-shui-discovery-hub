77a68e6afa39ae9064622afd9d9ec461cd27de90
myfengshuireport <myfengshuireport@gmail.com>
2025-07-23 00:26:27 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,22 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// src/App.tsx
+import HeroSection from "@/components/HeroSection";
+import FeaturesSection from "@/components/FeaturesSection";
+import CTASection from "@/components/CTASection";
+import Header from "@/components/Header"; // if you have one
+import Footer from "@/components/Footer"; // optional
 
-createRoot(document.getElementById("root")!).render(<App />);
+const App = () => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-grow">
+        <HeroSection />
+        <FeaturesSection />
+        <CTASection />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
